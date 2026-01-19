73ae6a75a7b55b921330c94604fc5fc0631c20cc
Eraki <moeraki@deloitte.com>
2026-01-19 17:35:45 +0200

    upd: rg module

---
+++ tf-bootstrap-modules/modules/rg/output.tf
@@ -0,0 +1,9 @@
+# list all output
+output "resouce_group_id" {
+  description = "ID of the created resource group"
+  value       = azurerm_resource_group.this.id
+}
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.this.name
+}
