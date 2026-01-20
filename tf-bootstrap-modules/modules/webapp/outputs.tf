659b7cb368b021719aeee4ed428c21d12c6c2495
Eraki <moeraki@deloitte.com>
2026-01-20 19:54:15 +0200

    add: configure webapp nsg

--- tf-bootstrap-modules/modules/webapp/outputs.tf
+++ tf-bootstrap-modules/modules/webapp/outputs.tf
@@ -12,3 +12,8 @@ output "webapp_id" {
   description = "The ID of the Web App"
   value       = azurerm_linux_web_app.app_service.id
 }
+
+output "network_security_group_id" {
+  description = "ID of the NSG created for the webapp"
+  value       = azurerm_network_security_group.webapp_nsg.id
+}
