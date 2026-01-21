6a6b4f0a853d6ce179ca34def3097bc36059ead5
Eraki <moeraki@deloitte.com>
2026-01-21 10:42:45 +0200

    add: az str backend

--- tf-environments/dev/provider.tf
+++ tf-environments/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate01"
+    storage_account_name = "tfstatest01"
     container_name       = "tfstatecontainer"
     key                  = "dev.terraform.tfstate"
   }
