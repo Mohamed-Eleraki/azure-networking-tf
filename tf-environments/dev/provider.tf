919f4cd45bf361e7f7e902a51d985b8d14985b38
Eraki <moeraki@deloitte.com>
2026-01-21 07:09:29 +0200

    add: backend

--- tf-environments/dev/provider.tf
+++ tf-environments/dev/provider.tf
@@ -1,3 +1,14 @@
+terraform { 
+  cloud { 
+    
+    organization = "HCP-remote-organization" 
+
+    workspaces { 
+      name = "Demo01" 
+    } 
+  } 
+}
+
 provider "azurerm" {
   features {}
   subscription_id                 = var.subscription_id
